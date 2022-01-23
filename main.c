a749c40aa02dcc32f3d13ed58b84f1770e8085e7
Maximal98 <55159959+Maximal98@users.noreply.github.com>
2022-01-23 13:41:49 +0100

    Update source to 1.0.2

--- main.c
+++ main.c
@@ -9,43 +9,52 @@
 #include <errno.h>
 
 char* getPidByName(char *proc_name, int verbose) {
+	//Setup for Getting the PID
 	if( verbose == 1 ) {
 		printf("\nAttempting to find PID of ");
 		printf(proc_name);
 		printf("\n");
 	}
-	FILE *fp;
+	FILE *CurrentProcfile;
 	char *pid = "-1";
 	int isPidNumber;
-	struct dirent *dir;
-	char *E;
-	char FullText[60] = "This is an Error!";
-	char Name[100];
+	struct dirent *procdir_ent;
+	char *CurrentPID;
+	char FullPath[60] = "This is an Error!";
+	char CurrentName[128];
+	int PID_counter = 0;
+	//open /proc/
+	//TODO: Error Handling here.
 	DIR *procdir = opendir("/proc");
 	if (procdir) {
-		while ((dir = readdir(procdir)) != NULL) {
-			if( verbose == 1 ) {
-				printf("Attempting to open /proc/\n");
-			}
-			E = dir->d_name; 
-			isPidNumber = isdigit(*E);
+		while ((procdir_ent = readdir(procdir)) != NULL) {
+			CurrentPID = procdir_ent->d_name; 
+			isPidNumber = isdigit(*CurrentPID);
 			if (isPidNumber != 0) {
-				strcpy(FullText, "/proc/");
-				strcat(FullText, E);
-				strcat(FullText, "/comm");
-				fp = fopen(FullText, "r");
-				if ( NULL == fp ) {
+				PID_counter++;
+				strcpy(FullPath, "/proc/");
+				strcat(FullPath, CurrentPID);
+				strcat(FullPath, "/comm");
+				CurrentProcfile = fopen(FullPath, "r");
+				if ( NULL == CurrentProcfile ) {
 					printf("Couldn't open file in /proc/ \n");
-					printf(FullText);
+					printf(FullPath);
 					printf("\n");
 					return "-2";
 				}
 				
-				/* Read and display data */
-				fscanf(fp, "%s", Name);
-				fclose(fp);
-				if(strcmp(Name, proc_name) == 0) {
-					pid = dir->d_name;
+				//read Data of current file in /proc/
+				//TODO: use fgets
+				fgets( CurrentName, 128, CurrentProcfile );
+				fclose( CurrentProcfile );
+				//Check if DE name is found.
+				if(strcmp(CurrentName, proc_name) == 0) {
+					if( verbose == 1 ) {
+						printf("Found Desktop after Checking ");
+						printf("%d", PID_counter);
+						printf(" Entries in /proc/.\n");
+					}
+					pid = procdir_ent->d_name;
 				}
 			}
 			
@@ -59,17 +68,20 @@ char* getPidByName(char *proc_name, int verbose) {
 extern char **environ;
 
 int run_DE(char *DE_name, int verbose) {
+	//Starting DE Setup
 	if( verbose == 1 ) {
 		printf("Trying to start DE\n");
 	}
 	pid_t ChildPid;
 	int status;
 	char *args[] = {DE_name, NULL};
+	//Starting and Error handling
 	status = posix_spawn(&ChildPid, DE_name, NULL, NULL, args, environ);
 	if ( status == 0 ) {
-		printf("started DE successfully!\n");
+		if( verbose == 1) {
+			printf("started DE successfully!\n");
+		}
 		wait(&status);
-		printf("%d", errno);
 	}
 	else {
 		printf("couldn't start program, returned error code ");
@@ -80,34 +92,43 @@ int run_DE(char *DE_name, int verbose) {
 }
 
 int main (int argc, char **argv) {
+	// Process arguments
 	int verbose = 0;
 	if( argc > 2) {
 		printf("Error: too many arguments.\n");
 		return 1;
 	}
-	if( strcmp(argv[1], "-h" ) == 0 || strcmp(argv[1], "--help" ) == 0 ) {
-		printf("UL-DM, there is only one option and its --help or --verbose (or -h and -v repsectivly.)\n");
-		return 1;
+	if( argc > 1) {
+		if( strcmp(argv[1], "-h" ) == 0 || strcmp(argv[1], "--help" ) == 0 ) {
+			printf("UL-DM, there is only one option and its --help or --verbose (or -h and -v repsectivly.)\n");
+			return 1;
+		}
+		else if( strcmp(argv[1], "-v" ) == 0 || strcmp(argv[1], "--verbose" ) == 0 ) {
+			printf("Running in Verbose mode.\n");
+			verbose = 1;
+		}
 	}
-	else if( strcmp(argv[1], "-v" ) == 0 || strcmp(argv[1], "--verbose" ) == 0 ) {
-		printf("Running in Verbose mode.\n");
-		verbose = 1;
-	}
-	
+	//Getting UID
 	uid_t RunningUID;
 	RunningUID = getuid();
+	if( verbose == 1 ) {
+		printf("Running UID is: ");
+		printf("%d", RunningUID);
+		printf("\n");
+	}
+	//checking if the program is running as Root
 	if ( RunningUID == 0 ) {
 		printf("Running as Root. Quitting. \n");
 		return 0;
 	}
-	printf("%d", RunningUID);
-	printf("\n");
+	//Setup Variables for Starting
 	char *Return;
 	FILE *ConfigFile;
 	char DEProcess[128];
 	char DEStarter[128];
 	int line = 0;
 
+	//Loading Configuration and Error handling
 	if( verbose == 1 ) {
 		printf("Loading config file.\n");
 	}
@@ -116,25 +137,20 @@ int main (int argc, char **argv) {
 		printf("There was an error opening the Config file. Check if it exists and this user has read access to it.\n");
 		return 1;
 	}
-	if( fgets(DEProcess, 128, ConfigFile) != 0) {
-		printf("Couldn't read config! failed with error: ");
-		printf("%d", errno);
-		printf("!\n");
-	}
-	if( fgets(DEStarter, 128, ConfigFile) != 0) {
-		printf("Couldn't read config! failed with error: ");
-		printf("%d", errno);
-		printf("!\n");
-	}
+	fgets(DEProcess, 128, ConfigFile);
+	fgets(DEStarter, 128, ConfigFile);
 	DEProcess[strcspn(DEProcess, "\n")] = 0;
 	DEStarter[strcspn(DEStarter, "\n")] = 0;
 
+	//Get PID of DE, if it returns -1, its not running.
+	//(this was a huge pain in the ass)
+	//And start if its not running.
 	Return = getPidByName(DEProcess, verbose);
 	if ( strcmp(Return, "-2") == 0 ) {
 		return(1);
 	}
 
-	if (verbose = 1) {
+	if (verbose == 1) {
 		printf("\nGetting PID was Successfull! Its: ");
 	}
 	if ( strcmp(Return, "-1") == 0) {
